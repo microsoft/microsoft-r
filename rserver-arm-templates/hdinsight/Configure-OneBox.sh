927baabd6c1b1d1f417a3e9bc706bdb55da37969
ramch <ramch@microsoft.com>
2017-05-12 10:54:55 -0700

    hdinsight onebox script

--- rserver-arm-templates/hdinsight/Configure-OneBox.sh
+++ rserver-arm-templates/hdinsight/Configure-OneBox.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-password=$1
-/usr/local/bin/dotnet /usr/lib64/microsoft-r/rserver/o16n/9.1.0/Microsoft.RServer.Utils.AdminUtil/Microsoft.RServer.Utils.AdminUtil.dll -silentoneboxinstall "$password"
+rm /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.WebAPI/appsettings.json
+wget -O /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.WebAPI/appsettings.json https://raw.githubusercontent.com/Microsoft/microsoft-r/master/rserver-arm-templates/hdinsight/WebAPI/appsettings.json 
+
+rm /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.BackEnd/appsettings.json
+wget -O /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.BackEnd/appsettings.json https://raw.githubusercontent.com/Microsoft/microsoft-r/master/rserver-arm-templates/hdinsight/BackEnd/appsettings.json 
+
+cp /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.WebAPI/autoStartScriptsLinux/* /etc/systemd/system/.
+cp /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.BackEnd/autoStartScriptsLinux/* /etc/systemd/system/.
+
+systemctl enable frontend
+systemctl enable rserve
+systemctl enable backend
+
+systemctl start frontend
+systemctl start rserve
+systemctl start backend
