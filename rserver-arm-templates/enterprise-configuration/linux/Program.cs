b5b6431ef674e671fda7aa4e56b40bf340d264aa
ramch <ramch@microsoft.com>
2017-05-22 15:40:28 -0700

    install cert

---
+++ rserver-arm-templates/enterprise-configuration/linux/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace InstallCert
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
+            var cert = new X509Certificate2("./merged.pfx");
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(cert);
+        }
+    }
+}
