b3eb2a1e6b861ea7a149f7aaa7ea93988d4bbd10
Ramkumar Chandrasekaran <ramnov@users.noreply.github.com>
2017-09-15 01:13:45 +0000

    Enterprise Configuration Template with SQL on Linux DB as WebNode backend (#20)
    * enterprise architecture with sql on linux DB backend
    
    * storage account fixes
    
    * removing web node restart
    
    * sql on linux script bug fixes
    
    * sql on linux script yum update
    
    * update mssql-server package

---
+++ rserver-arm-templates/enterprise-configuration/windows-sqlonlinux-azure/ConfigureSqlOnLinux.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Script Source : https://docs.microsoft.com/en-us/sql/linux/sample-unattended-install-redhat
+
+password=$1
+
+sudo yum update -y mssql-server
+
+echo Running mssql-conf setup...
+sudo MSSQL_SA_PASSWORD=$password /opt/mssql/bin/mssql-conf -n setup accept-eula
+
+echo Installing mssql-tools and unixODBC developer...
+sudo ACCEPT_EULA=Y yum install -y mssql-tools unixODBC-devel
+
+# Add SQL Server tools to the path by default:
+echo Adding SQL Server tools to your path...
+echo PATH="$PATH:/opt/mssql-tools/bin" >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+
+# Configure firewall to allow TCP port 1433:
+echo Configuring firewall to allow traffic on port 1433...
+sudo firewall-cmd --zone=public --add-port=1433/tcp --permanent
+sudo firewall-cmd --reload
+
+# Connect to server and get the version:
+counter=1
+errstatus=1
+while [ $counter -le 5 ] && [ $errstatus = 1 ]
+do
+  echo Waiting for SQL Server to start...
+  sleep 5s
+  /opt/mssql-tools/bin/sqlcmd \
+    -S localhost \
+    -U SA \
+    -P $password \
+    -Q "SELECT @@VERSION" 2>/dev/null
+  errstatus=$?
+  ((counter++))
+done
+
+# Display error if connection failed:
+if [ $errstatus = 1 ]
+then
+  echo Cannot connect to SQL Server, installation aborted
+  exit $errstatus
+fi
