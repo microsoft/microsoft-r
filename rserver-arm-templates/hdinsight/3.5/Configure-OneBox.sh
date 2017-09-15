456f14139040501317f7b228dde4d05cb2d508cb
Ramkumar Chandrasekaran <ramnov@users.noreply.github.com>
2017-09-15 15:06:34 -0700

    proper links for appsettings.json
    proper links for appsettings.json

--- rserver-arm-templates/hdinsight/3.5/Configure-OneBox.sh
+++ rserver-arm-templates/hdinsight/3.5/Configure-OneBox.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 rm /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.WebAPI/appsettings.json
-wget -O /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.WebAPI/appsettings.json https://raw.githubusercontent.com/Microsoft/microsoft-r/master/rserver-arm-templates/hdinsight/WebAPI/appsettings.json 
+wget -O /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.WebAPI/appsettings.json https://raw.githubusercontent.com/Microsoft/microsoft-r/master/rserver-arm-templates/hdinsight/3.5/WebAPI/appsettings.json 
 
 rm /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.BackEnd/appsettings.json
-wget -O /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.BackEnd/appsettings.json https://raw.githubusercontent.com/Microsoft/microsoft-r/master/rserver-arm-templates/hdinsight/BackEnd/appsettings.json 
+wget -O /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.BackEnd/appsettings.json https://raw.githubusercontent.com/Microsoft/microsoft-r/master/rserver-arm-templates/hdinsight/3.5/BackEnd/appsettings.json 
 
 cp /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.WebAPI/autoStartScriptsLinux/* /etc/systemd/system/.
 cp /usr/lib64/microsoft-deployr/9.0.1/Microsoft.DeployR.Server.BackEnd/autoStartScriptsLinux/* /etc/systemd/system/.
