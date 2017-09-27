127baa25687b3ef9a34c732c5d6e37e848d669c3
Ramkumar Chandrasekaran <ramch@microsoft.com>
2017-09-27 11:30:39 -0700

    adding ubuntu14 for mlserver onebox

--- mlserver-arm-templates/one-box-configuration/linux/Configure-OneBox.sh
+++ mlserver-arm-templates/one-box-configuration/linux/Configure-OneBox.sh
@@ -5,7 +5,9 @@ password=$1
 if [ -f /etc/debian_version ]; then
   apt-get update
   apt-get -y install apt-transport-https
-  curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > ./microsoft-prod.list
+  distrib_release=`cat /etc/*release | grep -i DISTRIB_RELEASE`
+  IFS='=' read -r -a u_version <<< "$distrib_release"
+  curl "https://packages.microsoft.com/config/ubuntu/${u_version[1]}/prod.list" > ./microsoft-prod.list
   cp ./microsoft-prod.list /etc/apt/sources.list.d/
   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
   cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
