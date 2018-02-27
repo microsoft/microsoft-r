9ba619e39fe9ede3686dd065a01c9fe6e995c88b
Ramkumar Chandrasekaran <ramch@microsoft.com>
2018-02-27 15:22:30 -0800

    run cli as admin

--- mlserver-arm-templates/one-box-configuration/windows/Configure-Onebox.ps1
+++ mlserver-arm-templates/one-box-configuration/windows/Configure-Onebox.ps1
@@ -2,18 +2,12 @@ param (
     [string]$password
 )
 
-$psi = New-Object System.Diagnostics.ProcessStartInfo;
-$psi.FileName = "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd";
-$psi.Arguments = "ml admin --help"
-$psi.WorkingDirectory = "C:\Program Files\Microsoft\ML Server";
-$psi.UseShellExecute = $false
-$p = [System.Diagnostics.Process]::Start($psi);
-$p.WaitForExit();
-
+echo $env:UserName
 $psi = New-Object System.Diagnostics.ProcessStartInfo;
 $psi.FileName = "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd";
 $psi.Arguments = "ml admin node setup --onebox --admin-password ""$password"" --confirm-password ""$password"""
 $psi.WorkingDirectory = "C:\Program Files\Microsoft\ML Server";
 $psi.UseShellExecute = $false
+$psi.Verb = "runas";
 $p = [System.Diagnostics.Process]::Start($psi);
 $p.WaitForExit();
