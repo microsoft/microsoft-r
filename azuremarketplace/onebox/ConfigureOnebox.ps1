91cd2992e217f7c1154f2a58d863e329b53a8beb
Ramkumar Chandrasekaran <ramch@microsoft.com>
2018-04-07 00:00:09 -0700

    adding marketplace image code

---
+++ azuremarketplace/onebox/ConfigureOnebox.ps1
@@ -0,0 +1,17 @@
+param (
+    [string]$oneboxpassword
+)
+
+$psi = New-Object System.Diagnostics.ProcessStartInfo;
+$psi.FileName = "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd";
+$psi.Arguments = "ml admin bootstrap --admin-password ""$oneboxpassword"" --confirm-password ""$oneboxpassword"""
+$psi.WorkingDirectory = "C:\Program Files\Microsoft\ML Server";
+$psi.UseShellExecute = $false
+$psi.RedirectStandardOutput = $true
+$psi.RedirectStandardError = $true
+$p = [System.Diagnostics.Process]::Start($psi);
+$poutput = $p.StandardOutput.ReadToEnd();
+$perror = $p.StandardError.ReadToEnd();
+$p.WaitForExit();
+Write-Output $poutput
+Write-Output $perror
