61c3f4e5db59c244b84caa84dfc2dc3661e4fbc5
Ramkumar Chandrasekaran <ramch@microsoft.com>
2018-08-07 18:31:02 -0700

    fixing dsvm 9.3 arm template windows

---
+++ mlserver-arm-templates/enterprise-configuration/windows-dsvm-sqlserver/RunConfigureWebNode.ps1
@@ -0,0 +1,16 @@
+param (
+    [string]$script,
+    [string]$username,
+    [string]$password,
+    [string]$sqlServerConnectionString
+)
+
+$securepassword =  ConvertTo-SecureString "$password" -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$username", $securepassword)
+Enable-PSRemoting -Force
+Invoke-Command -FilePath $script -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList $password,$sqlServerConnectionString
+Disable-PSRemoting -Force -WarningAction SilentlyContinue
+winrm delete winrm/config/listener?address=*+transport=HTTP
+Stop-Service winrm
+Set-Service -Name winrm -StartupType Disabled
+Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name LocalAccountTokenFilterPolicy -Value 0 -Type DWord
